e0776b60711b2c107d267685cd9b7ef047f4a7b2
Alyona Sabitskaya <trefilovaalyona@gmail.com>
2021-01-15 14:35:06 +0300

    custom button added

--- sportprogramm/UserModule/UserProgramms/DetailedProgramm/View/CustomViews/DailyTrainingCell.swift
+++ sportprogramm/UserModule/UserProgramms/DetailedProgramm/View/CustomViews/DailyTrainingCell.swift
@@ -135,12 +135,9 @@ class DailyTrainingCell: UITableViewCell {
                 descriptionOfSetLabel.font = UIFont(name: "SF Pro Text", size: 14)
                 descriptionOfSetLabel.text = "\(exercise.weight)кг/ \(exercise.numberOfReps)повт."
                 
-                let checkmarkButton = UIButton()
-                checkmarkButton.setImage(UIImage(systemName: "checkmark.square"), for: .normal)
+                let checkmarkButton = CheckBox(frame: CGRect.zero)
                 checkmarkButton.imageWith(color: .black, for: .normal)
-                checkmarkButton.setImage(UIImage(systemName: "checkmark.square.fill"), for: .selected)
-                checkmarkButton.imageWith(color: .black, for: .selected)
-                
+                self.becomeFirstResponder()
                 
                 bcView.addSubview(viewForNumberOfSet)
                 viewForNumberOfSet.addSubview(numberOfSetLabel)
@@ -165,7 +162,7 @@ class DailyTrainingCell: UITableViewCell {
                 
                 checkmarkButton.snp.makeConstraints { (make) in
                     make.centerY.equalTo(viewForNumberOfSet.snp.centerY)
-                    make.leading.equalTo(descriptionOfSetLabel.snp.trailing).offset(100)
+                    make.leading.equalTo(viewForNumberOfSet.snp.trailing).offset(150)
                     make.height.equalTo(16)
                     make.width.equalTo(16)
                 }
@@ -202,6 +199,7 @@ class DailyTrainingCell: UITableViewCell {
         
         return viewForSubCell
     }
+    
     
     func fillCellWithData(data: [Excersice], numberOfDay: Int) {
         let myContentView = UIView()
@@ -270,3 +268,39 @@ class DailyTrainingCell: UITableViewCell {
     
 }
 
+class CheckBox: UIButton {
+    
+    let uncheckedImage = UIImage(systemName: "checkmark.square")! as UIImage
+    let checkedImage = UIImage(systemName: "checkmark.square.fill")! as UIImage
+    var isChecked: Bool = false {
+        didSet{
+            if isChecked == true {
+                self.setImage(checkedImage, for: UIControl.State.normal)
+            } else {
+                self.setImage(uncheckedImage, for: UIControl.State.normal)
+            }
+        }
+    }
+    
+    override init(frame: CGRect) {
+       
+        super.init(frame: frame)
+        self.setImage(uncheckedImage, for: UIControl.State.normal)
+        self.imageWith(color: .blue, for: .normal)
+        self.addTarget(self, action:#selector(buttonClicked(sender:)), for: UIControl.Event.touchUpInside)
+        self.isChecked = false
+        
+    }
+    
+    @objc func buttonClicked(sender: UIButton) {
+        print("checkbox taped")
+            if sender == self {
+                isChecked = !isChecked
+            }
+        
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
